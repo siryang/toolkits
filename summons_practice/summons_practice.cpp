40cea80b182dbc7bdf8f22589f2a108973d5f509
siryang <sir_yangd@hotmail.com>
2014-07-10 17:57:50 +0800

    add KeyEvent responds

--- summons_practice/summons_practice.cpp
+++ summons_practice/summons_practice.cpp
@@ -4,9 +4,31 @@ summons_practice::summons_practice(QWidget *parent, Qt::WFlags flags)
 	: QMainWindow(parent, flags)
 {
 	ui.setupUi(this);
+	m_sum = 100;
+	m_num = 0;
+
+	update();
 }
+
 
 summons_practice::~summons_practice()
 {
 
+}
+
+void summons_practice::keyPressEvent(QKeyEvent  *event) 
+{
+	QString customNum = ui.CustomId->text();
+	switch (event->key())
+	{
+	}
+	m_num++;
+	update();
+}
+
+void summons_practice::update()
+{
+	ui.CustomId->setText("");
+	ui.CustomSum->setText(QString::number(m_sum));
+	ui.CurrentNum->setText(QString::number(m_num));
 }
--- summons_practice/summons_practice.h
+++ summons_practice/summons_practice.h
@@ -1,6 +1,7 @@
 #ifndef SUMMONS_PRACTICE_H
 #define SUMMONS_PRACTICE_H
 
+#include <QKeyEvent> 
 #include <QtGui/QMainWindow>
 #include "ui_summons_practice.h"
 
@@ -12,8 +13,13 @@ public:
 	summons_practice(QWidget *parent = 0, Qt::WFlags flags = 0);
 	~summons_practice();
 
+	void keyPressEvent(QKeyEvent *event);
 private:
+	void update();
+
 	Ui::summons_practiceClass ui;
+	size_t m_num;
+	size_t m_sum;
 };
 
 #endif // SUMMONS_PRACTICE_H
