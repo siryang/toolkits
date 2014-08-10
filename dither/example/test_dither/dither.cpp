896888ef478aacb1eae08458f990fa5c9fab9b94
siryang <sir_yangd@hotmail.com>
2014-08-10 13:41:59 +0800

    refactor

--- dither/example/test_dither/dither.cpp
+++ dither/example/test_dither/dither.cpp
@@ -365,7 +365,7 @@ static void _PngEncoder_destruct(png_struct* png_ptr, png_info* info_ptr)
 void process(PixelType* buffer, int width, int height, int pitchInPixel, BOOL dither)
 {
 	//PixelType palette[] = {Pixel_fromX8R8G8B8(0), Pixel_assemblePixel4(250, 79, 40, 255), Pixel_assemblePixel4(221, 30, 22, 255)};
-	int maxPaletteNum = 64;
+	int maxPaletteNum = 32;
 	PixelType* palette = (PixelType*)malloc(sizeof(PixelType) * maxPaletteNum);
 	int paletteNum;
 	int x, y;	
@@ -431,10 +431,9 @@ static void render()
 			PixelType* buffer;
 			Handle image = Surface_alloc();
 			Rect srcArea;
-			Surface_loadExt(image, L"map4.png"); 
+			//Surface_loadExt(image, L"map4.png"); 
 			//Surface_loadExt(image, L"lena.png"); 
-			
-			//Surface_loadExt(image, L"dog.jpg"/*L"baboon.png"*/); 
+			Surface_loadExt(image, L"baboon.png"); 
 			Surface_getArea(image, &srcArea);
 
 			// draw original image.
@@ -442,7 +441,7 @@ static void render()
 
 			buffer = (PixelType*)Surface_lock(image);
 
-			process(buffer, Rect_getWidth(srcArea), Rect_getHeight(srcArea), Rect_getWidth(srcArea), TRUE);
+			process(buffer, Rect_getWidth(srcArea), Rect_getHeight(srcArea), Rect_getWidth(srcArea), FALSE);
 
 			GDI_bitBlt(g_gdi, srcArea.right + 5, 0, image, &srcArea);
 
--- dither/example/test_dither/dither_shuffling.cpp
+++ dither/example/test_dither/dither_shuffling.cpp
@@ -36,10 +36,98 @@ inline bool lessPixelNodeKey(PixelNode left, PixelNode right)
 	return left.key < right.key;
 }
 
+//inline int lessPixel(const void* left, const void* right)
+//{
+//	PixelType& leftPixel = *(PixelType*)left;
+//	PixelType& rightPixel = *(PixelType*)right;
+//	if (leftPixel == rightPixel)
+//		return 0;
+//	
+//	if (leftPixel < rightPixel)
+//		return -1;
+//
+//	return 1;
+//}
+
+void testSortCost(PixelType* buffer, int width, int height, int pitchInPixel)
+{
+	clock_t start = clock();
+	// test
+	vector<PixelType> colors;
+	colors.resize(width * height);
+	for(int y = 0; y < height; y++)
+	{
+		for(int x = 0; x < width; x++)
+		{
+			colors[y * width + x] = buffer[y * pitchInPixel + x];
+		}
+	}
+
+	clock_t testStart = clock();
+	// sort
+	//sort(colors.begin(), colors.end());
+
+	// sort by heap;
+	//make_heap(colors.begin(), colors.end());
+	//sort_heap(colors.begin(), colors.end());
+
+	// qsort
+	//qsort(&colors[0], colors.size(), sizeof(PixelType), lessPixel);
+	clock_t sortCostTest = clock();
+	printf("test cost: %3f \n", (sortCostTest - start) / 1000.0f);
+	printf("sort cost: %3f \n", (sortCostTest - testStart) / 1000.0f);
+}
+
+void makePalette(PixelNode* pixelStart, int pixelNumber, PixelType* palettes, int maxPaletteNum)
+{
+	PixelType* pattle = palettes;
+	int unitNumber;
+	int r = 0,g = 0,b = 0;
+	PixelNode* pixelEnd = pixelStart + pixelNumber;
+	PixelNode* pixel = pixelStart;
+	for (int paletteIdx = 0; paletteIdx < maxPaletteNum; paletteIdx++)
+	{
+		if (pixelNumber == 0){
+			maxPaletteNum = paletteIdx;
+			break;
+		}
+
+		unitNumber = (paletteIdx == maxPaletteNum - 1)? pixelNumber
+			: (pixelNumber / (maxPaletteNum - paletteIdx));
+
+		const PixelNode* lp;
+		int i = 0;
+		for (; i < unitNumber; i++)
+		{
+			lp = pixel++;
+			// contribution to pattle color
+			r += Pixel_getR(lp->color);
+			g += Pixel_getG(lp->color);
+			b += Pixel_getB(lp->color);
+		}
+
+		if (paletteIdx != maxPaletteNum - 1)
+		{
+			for (; pixel != pixelEnd && lp->color == pixel->color; i++)
+			{
+				lp = pixel++;
+				r += Pixel_getR(lp->color);
+				g += Pixel_getG(lp->color);
+				b += Pixel_getB(lp->color);
+			}
+		}
+
+		r /= i; g /= i; b /= i;
+		*pattle++ = Pixel_assemblePixel(r, g, b);
+		pixelNumber -= i;
+		if (pixelNumber < 0)
+			__debugbreak();
+	}
+}
+
 int extractPalete_shuffling( PixelType* buffer, int width, int height, int pitchInPixel, PixelType* palettes, int maxPaletteNum )
 {	
 	clock_t start = clock();
-
 	// 1.get freq
 	int pixelNumber = width * height;
 	vector<PixelNode> pixels;
@@ -60,38 +148,14 @@ int extractPalete_shuffling( PixelType* buffer, int width, int height, int pitch
 	}
 	clock_t copyEnd = clock();
 
+	// 能否不排序来解决?
 	// 2.sort color
 	sort(pixelStart, pixelEnd, lessPixelNodeKey);
 	clock_t sortEnd = clock();
 
-	PixelType* pattle = palettes;
-	int r = 0,g = 0,b = 0;
-	int unitNumber = pixelNumber / maxPaletteNum;
-	pixel = pixelStart;
-	for (int paletteIdx = 0; paletteIdx < maxPaletteNum; paletteIdx++)
-	{
-		if (paletteIdx == maxPaletteNum - 1)
-		{
-			unitNumber += pixelNumber % maxPaletteNum;
-		}
+	// make pattle
+	makePalette(pixelStart, pixelNumber, palettes, maxPaletteNum);
 
-		for (int i = 0; i < unitNumber; i++)
-		{
-			const PixelNode& lp = *pixel++;
-			if (pixel > pixelEnd)
-			{
-				__debugbreak();
-			}
-			r += Pixel_getR(lp.color);
-			g += Pixel_getG(lp.color);
-			b += Pixel_getB(lp.color);
-		}
-
-		r /= unitNumber;
-		g /= unitNumber;
-		b /= unitNumber;
-		*pattle++ = Pixel_assemblePixel(r, g, b);
-	}
 	clock_t getPattleEnd = clock();
 
 #define GetFloatCost(start, end) ((end) - (start)) / 1000.0f
